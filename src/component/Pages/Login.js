19370b689bd2677d7c50a67cf5e9c481c2620dd7
ArunRathaur1 <arunrathaur92.6@gmail.com>
2023-12-21 21:54:06 +0530

    updated the website

--- src/component/Pages/Login.js
+++ src/component/Pages/Login.js
@@ -1,34 +1,33 @@
 import React from "react";
+import '../css/login.css';
 function Login(){
     return(
         <>
-        <form action="action_page.php" style={{border:"1px solid #ccc"}}>
-        <div class="container">
-    <h1>Sign Up</h1>
+        <div class="image1">
+        <div className="Form" style={{position:"relative",top:"1cm",left:"15%",borderRadius:"10px"}}>
+        <form action="submit" >
+        <div class="container" style={{height:"12cm"}}>
+    <h1 style={{textAlign:"center"}}>Sign Up</h1>
     <p>Please fill in this form to create an account.</p>
     <hr/>
 
     <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required/>
+    <input type="text" placeholder="Enter Email" name="email" required className="box"/>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required/>
+    <input type="password" placeholder="Enter Password" name="psw" required className="box"/>
 
     <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
-
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style={{marginBottom:"15px"}}/> Remember me
-    </label>
-
-    <p>By creating an account you agree to our <a href="#" style={{color:"dodgerblue"}}>Terms & Privacy</a>.</p>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required className="box"/>
 
     <div class="clearfix">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
-    </div>
+    <p><button type="submit" class="signupbtn">Sign Up</button></p>
+      <p><button type="button" class="cancelbtn" style={{marginBottom:"80px"}}>Cancel</button></p>
+      </div>
   </div>
 </form>
+</div>
+</div>
         </>
     )
 }
